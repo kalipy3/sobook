2778863f2ce6e9ed80442361ec7367286b145bc7
kalipy <3069087972@qq.com>
2020-05-19 20:23:39 +0800

    express_v0.3 删除app1.js app2.js...

--- app.js
+++ app.js
@@ -6,41 +6,26 @@
  */
 
 let express = require("express")
+let path = require('path')
 let mysql = require("mysql")
-let app = express()
+var app = express()
 let sqlQuery = require("./lcMysql")
-//使用模板来渲染页面
-let ejs = require('ejs')
-//将模板引擎与express应用相关联
-app.set('views',"views")//设置视图对应的目录
-app.set("view engine","ejs")//设置默认的模板引擎
-app.engine("ejs",ejs.__express)//定义模板引擎
+
+//view engine setup
+app.set('views',path.join(__dirname,'views'))
+app.set("view engine","ejs")
+
+//设置静态目录
+app.use(express.static(path.join(__dirname,'public')))
 
 
-app.get("/",async (req,res)=>{
-    //插入变量
+app.get('/',async (req,res)=>{
+    let strSql = "select id,bookname,bookimg,author,cataory from book limit 0,28"
+    let result = await sqlQuery(strSql)//需要加async await,不然输出结果为：Promise{<pending>}
     let options = {
-        title:"lcbook首页",
-        articleTitle:"<h1>文章标题</h1>"
+        books:Array.from(result)
     }
-    res.render('index.ejs',options)
-
-})
-app.get("/tj",async (req,res)=>{  
-    //条件
-    let options = {
-        "username":"小明",
-        "gender":"男"
-    }
-    res.render('condition.ejs',options) 
-})
-app.get("/xh",async (req,res)=>{
-    //循环
-    let stars = ["蔡徐坤","南怀瑾","悉达多","lc"]
-    let options = {
-        stars
-    }
-    res.render('xh.ejs',options)
+    res.render('bookIndex.ejs',options)
 })
 
 module.exports = app
