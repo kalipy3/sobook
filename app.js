f4c5aa576cef69abca75060c0d50e27917e82152
kalipy <3069087972@qq.com>
2020-05-20 11:14:50 +0800

    express_v0.6 1.现实分类 2.实现搜索功能

--- app.js
+++ app.js
@@ -23,11 +23,39 @@ app.get('/',async (req,res)=>{
     let strSql = "select id,bookname,bookimg,author,cataory from book limit 0,28"
     let result = await sqlQuery(strSql)//需要加async await,不然输出结果为：Promise{<pending>}
     let options = {
-        books:Array.from(result)
+        books:Array.from(result),
+        cataorys:await getCataory()
     }
     res.render('bookIndex.ejs',options)
 })
 
+//设置搜索路由
+app.get('/search/:searchKey',async (req,res)=>{
+    let sqlStr = "select id,bookname,bookimg,author,cataory from book where bookname like '%"+req.params.searchKey+"%' or author like '%"+req.params.searchKey+"%' limit 0,28"
+    let result = await sqlQuery(sqlStr)
+
+    let options = {
+        books:Array.from(result),
+        cataorys:await getCataory()
+    }
+    res.render('bookIndex.ejs',options)
+
+})
+
+
+//设置分类页面的路由
+app.get("/cataory/:cataoryid",async (req,res)=>{
+    let sqlStr = "select id,bookname,bookimg,author,cataory from book where cataory in (select cataory from cataory where id = ?) limit 0,28"
+    let arr = [req.params.cataoryid]
+    let result = await sqlQuery(sqlStr,arr)
+
+    let options = {
+        books:Array.from(result),
+        cataorys:await getCataory()
+    }
+    res.render('bookIndex.ejs',options)
+
+})
 
 app.get('/books/:bookid',async (req,res)=>{
     let strSql = "select * from book where id = ?"
@@ -41,5 +69,12 @@ app.get('/books/:bookid',async (req,res)=>{
     res.render('bookInfo.ejs',options)
 })
 
+
+async function getCataory(){
+    //获取所有分类
+    let sqlStr = "select * from cataory"
+    let result = await sqlQuery(sqlStr)
+    return Array.from(result)
+}
 
 module.exports = app
